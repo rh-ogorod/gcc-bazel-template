08cd09923eb3774d170bf2ca83e33eae1119e8a7
ramblehead <v.rybynok@gmail.com>
2022-07-27 00:12:11 +0100

    Checkpoint

--- packages/hello-world/src/main.cpp
+++ packages/hello-world/src/main.cpp
@@ -1,10 +1,14 @@
 // Hey Emacs, this is -*- coding: utf-8 -*-
 
+#include <cstdint>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
   // std::locale loc(std::locale(), new nan_num_put);
   // std::cout.imbue(loc);
+
+  std::uint16_t a = 10;
+  std::uint8_t b = a;
 
   std::cout << "Hello World!" << std::endl;
 
